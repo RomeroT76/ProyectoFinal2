99fef5c1189aa2edb7e174e85213ee3b8bb6eb20
Jorgemrqz <estebamarquez2702@gmail.com>
2024-07-25 11:46:11 -0500

    agregando nuevas vistas y cambiando rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,9 @@ import { BuscadorComponent } from './pages/buscador/buscador.component';
 import { CatalogoComponent } from './pages/catalogo/catalogo.component';
 import { LoginComponent } from './pages/login/login.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
+import { EdituserComponent } from './pages/edituser/edituser.component';
+import { EdituseradminComponent } from './pages/edituseradmin/edituseradmin.component';
+import { FavoriteComponent } from './pages/favorite/favorite.component';
 
 export const routes: Routes = [
     {path: 'login/register', title: 'Registro', component:RegisterComponent},
@@ -13,5 +16,8 @@ export const routes: Routes = [
     {path: 'catalogo', title: 'Catalogo', component:CatalogoComponent},
     {path: 'register', title: 'Registro', component:RegisterComponent},
     {path: 'login', title: 'Iniciar Sesion', component:LoginComponent},
+    {path: 'edituser', title: 'Editar usuario', component:EdituserComponent},
+    {path: 'edituseradmin', title: 'Editar usuario admin', component:EdituseradminComponent},
+    {path: 'favorite', title: 'Mis libros', component: FavoriteComponent},
     {path: '' , component:InicioComponent}
 ];
---
+++ src/app/pages/edituser/edituser.component.ts
@@ -0,0 +1,93 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Usuario } from '../../domain/Usuario';
+import { RouterLink } from '@angular/router';
+import { ComunicacionService } from '../../services/comunicacion.service';
+
+@Component({
+  selector: 'app-edituser',
+  standalone: true,
+  imports: [FormsModule, RouterLink],
+  templateUrl: './edituser.component.html',
+  styleUrl: './edituser.component.scss'
+})
+export class EdituserComponent {
+  usuario?: Usuario
+  name: string;
+  userName: string;
+  mail: string;
+  rol: string;
+  password: string;
+  passwordC: string;
+  currentRol: string;
+  id: string;
+
+  constructor(private comunicacionService: ComunicacionService) {
+    this.name = '';
+    this.mail = '';
+    this.userName = '';
+    this.password = '';
+    this.passwordC = '';
+    this.rol = '';
+    this.currentRol = '';
+    this.id = '';
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.comunicacionService.suscribe().subscribe(data => {
+      this.currentRol = data;
+    });
+
+    this.comunicacionService.susbribeOUsuario().subscribe(data => {
+      this.name = data.name;
+      this.mail = data.mail;
+      this.userName = data.userName;
+      this.password = data.password;
+      this.rol = data.rol;
+    });
+
+    this.comunicacionService.suscribeOId().subscribe(data => {
+      this.id = data;
+    })
+  }
+
+  // registrar() {
+  //   this.usuario = new Usuario;
+  //   this.usuario.name = this.name;
+  //   this.usuario.userName = this.userName;
+  //   this.usuario.mail = this.mail;
+  //   this.usuario.password = this.password;
+  //   this.usuario.rol = 'client';
+  //   if (this.password === this.passwordC && this.usuario.name != '' && this.usuario.userName != '' && this.usuario.mail != '' && this.usuario.rol != '') {
+  //     this.fireStoreService.registrarUsuario(this.usuario);
+  //     alert('Usuario creado correctamente');
+  //     this.usuario = undefined;
+  //     this.name = '';
+  //     this.mail = '';
+  //     this.userName = '';
+  //     this.password = '';
+  //     this.passwordC = '';
+  //   } else {
+  //     alert('Error al crear el usuario verifique que los campos han sido llenados correctamente');
+  //   }
+  // }
+
+  // modificar() {
+  //   this.fireStoreService.borrarUsuarios(this.id);
+  //   this.usuario = new Usuario();
+  //   this.usuario.set(this.name, this.userName, this.mail, this.rol, this.password);
+
+  //   this.fireStoreService.registrarUsuario(this.usuario);
+  //   alert('Usuario modificado correctamente');
+  //   this.usuario = undefined;
+  //   this.name = '';
+  //   this.mail = '';
+  //   this.userName = '';
+  //   this.password = '';
+  //   this.passwordC = '';
+  //   this.rol = '';
+
+  // }
+}
