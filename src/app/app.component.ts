3555fc4014e3a69422c55abad89095418ff42547
RomeroT76 <robertoart76x@gmail.com>
2024-07-28 10:16:42 -0500

    Funcionalidad para mantener la sesion del usuario cuando se recargue l apagina del navegador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,33 +6,37 @@ import { NavBarUsersComponent } from './components/nav-bar-users/nav-bar-users.c
 import { NavBarAnyComponent } from './components/nav-bar-any/nav-bar-any.component';
 import { CommonModule } from '@angular/common';
 import { CoreServiceService } from './services/core-service.service';
+import { LoginServiceService } from './services/login-service.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent, NavBarUsersComponent, NavBarAnyComponent,CommonModule],
+  imports: [RouterOutlet, NavBarComponent, NavBarUsersComponent, NavBarAnyComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'proyecto-final';
-  role: string = 'GUEST';
+  role: string = '';
 
-  constructor(private coreService: CoreServiceService) { }
+  constructor(private coreService: CoreServiceService,
+    private loginService: LoginServiceService
+  ) { }
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    
 
     this.coreService.getRol().subscribe(
-        data => {
-          this.role = data;
-        }
+      data => {
+        this.role = data;
+      }
     );
+
+    this.getSessionData();
   }
 
-  getSessionData(): void {
-    
+  getSessionData() {
+    this.coreService.setRol(this.loginService.getPayload().role);
   }
 }
