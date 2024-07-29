e8740afcd75082b4fd738310331fe94b6ca567e3
Jorgemrqz <estebamarquez2702@gmail.com>
2024-07-29 02:15:05 -0500

    agregando funcionalidad

--- src/app/pages/catalogo/catalogo.component.ts
+++ src/app/pages/catalogo/catalogo.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { BookServiceService } from '../../services/book-service.service';
+import { RouterLink } from '@angular/router';
+import { Book } from '../../domain/Book';
 
 @Component({
   selector: 'app-catalogo',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './catalogo.component.html',
   styleUrl: './catalogo.component.scss'
 })
@@ -27,6 +29,27 @@ export class CatalogoComponent {
         this.catalog = res;
       },
       error: err => console.log(err)
+    });
+
+
+  }
+
+  deleteBook(id: number) {
+    this.bookService.deleteBook(id).subscribe({
+      next: () => {
+        this.getAddedBooks(); // Actualizar el catálogo después de la eliminación
+      },
+      error: err => console.error('Error deleting book', err)
+    });
+  }
+
+   updateBook(book: Book) {
+    this.bookService.updateBook(book).subscribe({
+      next: updatedBook => {
+        console.log('Book updated successfully', updatedBook);
+        this.getAddedBooks(); // Actualizar el catálogo después de la actualización
+      },
+      error: err => console.error('Error updating book', err)
     });
   }
 }
