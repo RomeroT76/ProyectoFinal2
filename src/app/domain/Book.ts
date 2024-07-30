333a83185500f090642192b2a437af911af4325e
RomeroT76 <robertoart76x@gmail.com>
2024-07-30 18:51:00 -0500

    commit

--- src/app/domain/Book.ts
+++ src/app/domain/Book.ts
@@ -4,9 +4,5 @@ export class Book {
     genere?: string;
     image?: string;
     author?: string;
-<<<<<<< HEAD
-    availability?: String;
-=======
     availability?: string;
->>>>>>> 74c65b03e923b2aa0232c34ad020751d3e0d6d7b
 }
