c8dffca2ec289c4e427cd8b7ac25ef16a26dd595
Jorgemrqz <estebamarquez2702@gmail.com>
2024-07-30 21:34:59 -0500

    agregando reportes

--- src/app/pages/history/history.component.ts
+++ src/app/pages/history/history.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { LoansService } from '../../services/loans.service'; // Asegúrate de que la ruta sea correcta
-import { Loan } from '../../domain/Loan'; // Asegúrate de que la ruta sea correcta
+import { LoansService } from '../../services/loans.service';
+import { Loan } from '../../domain/Loan';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -12,11 +12,15 @@ import { CommonModule } from '@angular/common';
 })
 export class HistoryComponent implements OnInit {
   loans: Loan[] = [];
+  topUser: any[] = [];
+  topBook: any[] = [];
 
   constructor(private loansService: LoansService) {}
 
   ngOnInit(): void {
     this.loadLoans();
+    this.loadTopUser();
+    this.loadTopBook();
   }
 
   loadLoans(): void {
@@ -26,6 +30,30 @@ export class HistoryComponent implements OnInit {
       },
       error: (err) => {
         console.error('Error al obtener los préstamos:', err);
+      }
+    });
+  }
+
+  loadTopUser(): void {
+    this.loansService.getTopUser().subscribe({
+      next: (data: any[]) => {
+        console.log('Top User:', data);
+        this.topUser = data;
+      },
+      error: (err) => {
+        console.error('Error al obtener el usuario con más préstamos:', err);
+      }
+    });
+  }
+
+  loadTopBook(): void {
+    this.loansService.getTopBook().subscribe({
+      next: (data: any[]) => {
+        console.log('Top Book:', data);
+        this.topBook = data;
+      },
+      error: (err) => {
+        console.error('Error al obtener el libro más prestado:', err);
       }
     });
   }
